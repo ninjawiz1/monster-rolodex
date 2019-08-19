b29f9f39a3c729f67f60c2b3c282c9ab9ffbc61f
ninjawiz1 <ninjawiz1@gmail.com>
2019-08-19 14:10:46 +0530

    Updates

---
+++ precache-manifest.5b8af3954a5c8f98007d0028091e3b70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85f325829f401eae95c745e453f16fcf",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "4f3dac696908176049c8",
+    "url": "/monster-rolodex/static/css/main.c2529cee.chunk.css"
+  },
+  {
+    "revision": "e6f182edb81f551cb0c1",
+    "url": "/monster-rolodex/static/js/2.a4cf3094.chunk.js"
+  },
+  {
+    "revision": "4f3dac696908176049c8",
+    "url": "/monster-rolodex/static/js/main.502fc466.chunk.js"
+  },
+  {
+    "revision": "1f1fdb189afd82863836",
+    "url": "/monster-rolodex/static/js/runtime~main.80659f76.js"
+  }
+]);
